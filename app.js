4a88a0d397865e7052bbabd8c0620dd4529ad0c7
Himanshu <himanshu@localhost.localdomain>
2023-05-22 20:55:55 +0530

    ADDED MongoDB (Mongoose)

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-
+const mongoose = require("mongoose");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts=[];//global variable
+// let posts=[];//global variable
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,8 +17,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+
+mongoose.connect('mongodb+srv://new-user:08cgt7hNen0eX2Rz@todo.388n0tg.mongodb.net/Blog-Site?retryWrites=true&w=majority');
+  
+  const blogsSchema = new mongoose.Schema({
+    title: String,
+    post:String
+  });
+
+  const Blog = mongoose.model('Blog', blogsSchema);
+  
+
+
 app.get("/",function (req,res) {  
-  res.render("home",{startingContent:homeStartingContent,posts:posts});
+  Blog.find({}).then(function (items) {
+    res.render("home", {startingContent:homeStartingContent,posts:items});
+    });
+//  res.render("home",{startingContent:homeStartingContent,posts:posts});
   
 });
 
@@ -36,16 +51,14 @@ app.get("/contact",function (req,res) {
 
 
 app.get("/compose",function (req,res) {
+
   res.render("compose");
 });
 
 app.post("/compose",function (req,res) {
-  const post={
-    title:req.body.postTitle,
-    content:req.body.postBody
-  };
-  
-  posts.push(post);
+
+  Blog.insertMany({title:req.body.postTitle,post:req.body.postBody});
+
   res.redirect("/");
 
 });
@@ -53,22 +66,16 @@ app.post("/compose",function (req,res) {
 
 app.get("/posts/:postName",function (req,res) {
  let requestedTitle=_.lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    let storedTitle=_.lowerCase(post.title);
-    
-  if(requestedTitle===storedTitle){
-    console.log("Match found");
-    res.render("post",{title:post.title,content:post.content});
-    
-  }
-
-    });
+ Blog.find({title:requestedTitle}).then(function (oldBlog) {
+   console.log("Match found");console.log(oldBlog[0].toObject().title);
+    res.render("post",{title:oldBlog[0].toObject().title,content:oldBlog[0].toObject().post});  
+  });  
 });
 
 
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
